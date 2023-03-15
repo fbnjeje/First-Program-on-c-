be5e521fb124e65506ea4457d500afb78d62ea64
Neonx <fabiangomes2015@gmail.com>
2023-03-15 16:37:03 -0500

    Add new program using matrices

---
+++ 1.Arreglos/main.cpp
@@ -0,0 +1,16 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    // Write C++ code here
+    
+    char letra[] = {'a','e','i','o','u'};
+    
+    
+    
+    cout << letra[1];
+
+    return 0;
+}
